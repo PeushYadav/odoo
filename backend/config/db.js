9daa0155ffeb735bbee3439a938beb584b4b4038
PeushYadav <peushyadav07@gmail.com>
2025-07-12 11:45:39 +0530

    working jwt

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/jwtauth', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('✅ MongoDB Connected');
-  } catch (err) {
-    console.error('❌ MongoDB Connection Error:', err.message);
+    await mongoose.connect(
+      "mongodb+srv://peushyadav07:<HnGDiOrey7qmHcoN>@cluster0.euzb4z2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("✅ MongoDB Atlas connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
