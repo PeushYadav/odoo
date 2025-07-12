b7af3109b021969231330e4ccf4417dfd6de533c
Varchasv Sharma <varch.1906@gmail.com>
2025-07-12 12:46:21 +0530

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,17 @@ import { useState } from 'react';
 import Auth from './components/Auth/Auth';
 import Card from './components/Card/card';
 import LandingPage from './components/LandingPage';
+import { Navbar } from './components/Card/navbar';
 
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Auth />
     <Card />
     <LandingPage/>
+    
     </>
   )
  
